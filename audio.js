60c2ef8c9cff24163de9e0bd364c4403961c9f2f
Tom Aizenberg <tom.aizenberg@gmail.com>
2017-12-17 16:23:38 +0100

    add audio-based visual effects 🔊

---
+++ audio.js
@@ -0,0 +1,69 @@
+class AudioAnalyser {
+
+    constructor(time_smoothing, lower_threshold_db, upper_threshold_db, boomness_clip) {
+        this.running = false;
+        this.time_smoothing = time_smoothing;
+        this.lower_threshold_db = lower_threshold_db;
+        this.upper_threshold_db = upper_threshold_db;
+        this.boomness_clip = boomness_clip;
+        this.boom = 0;
+    }
+
+    run() {
+        navigator.getUserMedia(
+            {audio: true},
+            this.setup_audio_context.bind(this),
+            console.error
+        );
+    }
+
+    boomness() {
+        // returns float 0 - 1 with the amount of boom in the room
+        if (this.boom > this.boomness_clip) {
+            return this.boom;
+        } else {
+            return 0;
+        }
+    }
+
+    setup_audio_context(stream) {
+        const audio_context = new(window.AudioContext || window.webkitAudioContext)();
+
+        this.analyser = audio_context.createAnalyser();
+        this.analyser.fftSize = 32;
+        this.analyser.smoothingTimeConstant = this.time_smoothing;
+
+        const biquad_filter = audio_context.createBiquadFilter();
+        biquad_filter.type = "lowpass";
+        biquad_filter.frequency.value = 300;
+
+        const microphone = audio_context.createMediaStreamSource(stream);
+
+        biquad_filter.connect(this.analyser);
+        microphone.connect(biquad_filter);
+
+        console.log("AudioContext wired up, woof!");
+
+        this.running = true;    
+        this.process_audio();
+    }
+
+    process_audio() {
+        if (!this.running) return true;
+        requestAnimationFrame(this.process_audio.bind(this));
+        const fft_data = new Float32Array(this.analyser.frequencyBinCount);
+        this.analyser.getFloatFrequencyData(fft_data);
+        this.boom = this.normalize_db(fft_data[1]);
+    }
+
+    stop() {
+        this.running = false;
+    }
+
+    normalize_db(val_db) {
+        let val_limited = Math.min(Math.max(val_db, this.lower_threshold_db), this.upper_threshold_db);
+        let val_offset = (val_limited - this.lower_threshold_db);
+        let val_normalized =  val_offset / (this.upper_threshold_db - this.lower_threshold_db);
+        return val_normalized;
+    }
+}
